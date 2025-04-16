452813ba48ed9895af3d40cec5efe69b036e215c
Masaharu Tashiro <tasshi.me@gmail.com>
2025-04-17 03:48:46 +0900

    build: fix bundle script

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -4,12 +4,12 @@ import {glob} from 'glob'
 import path from "node:path"
 
 const inputs = (
-    await glob(path.resolve(__dirname, "..", "kintone/**/openapi.yaml"))
+    await glob(path.resolve(__dirname, "..", "kintone/*/openapi.yaml"))
 ).map((p) => path.relative(path.join(__dirname, ".."), p));
 
 for (const input of inputs) {
     const outputPath = path.join(path.dirname(input), "bundled", "openapi.yaml")
-    await $`mkdir ${path.dirname(outputPath)}`
+    await $`mkdir -p ${path.dirname(outputPath)}`
     echo`${input} -> ${outputPath}`
     await $`redocly bundle ${input} -o ${outputPath}`
 }
